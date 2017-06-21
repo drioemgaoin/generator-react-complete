5d3f26fefcc81aa1028529a2b4d1f30bc6d2ec0b
drioemgaoin <romain_diegoni@hotmail.com>
2017-06-21 16:59:55 +0100

    WIP: Setup choice between javascript and typescript

--- generators/app/index.js
+++ generators/app/index.js
@@ -11,57 +11,66 @@ const _ = require('lodash');
 const utils = require('../../utils/all');
 
 module.exports = class extends Generator {
-    constructor(args, options) {
-        super(args, options);
+  constructor(args, options) {
+    super(args, options);
 
-        this.option('skip-welcome-message', {
-            desc: 'Skip the welcome message',
-            type: Boolean,
-            defaults: false
-        });
+    this.option('skip-welcome-message', {
+      desc: 'Skip the welcome message',
+      type: Boolean,
+      defaults: false
+    });
 
-        this.option('skip-install');
+    this.option('skip-install');
 
-        this.config.save();
+    this.config.save();
+  }
+
+  prompting() {
+    this.log(yosay('Welcome to the outstanding ' + chalk.red('generator-react-complete') + ' generator!'));
+
+    return this.prompt(prompts)
+    .then(answers => {
+      // Make sure to get the correct app name if it is not the default
+      if(answers.appName !== utils.yeoman.getAppName()) {
+        answers.appName = utils.yeoman.getAppName(answers.appName);
+      }
+
+      // Set needed global vars for yo
+      this.appName = answers.appName;
+      this.compiler = answers.compiler;
+
+      // Set needed keys into config
+      this.config.set('appName', this.appName);
+    });
+  }
+
+  writing() {
+    let devDependencies = [];
+
+    let compilerConfig = utils.config.getChoiceByKey('compiler', this.compiler);
+    if (compilerConfig) {
+      for(let dependency of compilerConfig.packages) {
+        devDependencies[dependency.name] = dependency.version;
+      }
+
+      this.fs.copyTpl(
+        this.templatePath('_' + compilerConfig.config),
+        this.destinationPath(compilerConfig.config)
+      );
     }
 
-    prompting() {
-        this.log(yosay('Welcome to the outstanding ' + chalk.red('generator-react-complete') + ' generator!'));
+    this.fs.copyTpl(
+      this.templatePath('_package.json'),
+      this.destinationPath('package.json'),
+      {
+        appName: this.appName,
+        devDependencies: devDependencies
+      }
+    );
 
-        return this.prompt(prompts)
-            .then(answers => {
-                // Make sure to get the correct app name if it is not the default
-                if(answers.appName !== utils.yeoman.getAppName()) {
-                    answers.appName = utils.yeoman.getAppName(answers.appName);
-                }
+  }
 
-                // Set needed global vars for yo
-                this.appName = answers.appName;
-                this.compiler = answers.compiler;
+  install() {
 
-                // Set needed keys into config
-                this.config.set('appName', this.appName);
-            });
-    }
-
-    writing() {
-        this.fs.copyTpl(
-            this.templatePath('_package.json'),
-            this.destinationPath('package.json'),
-            { appName: this.appName }
-        );
-
-        let compilerConfig = utils.config.getChoiceByKey('compiler', this.compiler);
-        if (compilerConfig) {
-          this.fs.copyTpl(
-              this.templatePath('_' + compilerConfig.config),
-              this.destinationPath(compilerConfig.config)
-          );
-        }
-
-    }
-
-    install() {
-
-    }
+  }
 };
